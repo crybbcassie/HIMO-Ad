8ba7f752c50f1b918f994852bafcf3977deb332f
Cassie <crybbcassie@gmail.com>
2022-08-12 17:11:10 +0300

    Release

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(Document).ready(function(){
+$(".bike__slider").slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+$(".slider__items").slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: true,
+});
+});
+
+
+
